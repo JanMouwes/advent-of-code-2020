1cfe00a92ec09d054ab05da898d1720f5f5351c6
JanMouwes <jan.mouwes@gmail.com>
2020-12-03 07:06:58 +0100

    Minor code clarity improvements

--- src/Day_3/TreeTrajectory.hs
+++ src/Day_3/TreeTrajectory.hs
@@ -1,10 +1,12 @@
 module Day_3.TreeTrajectory where
 
 part1 :: (Int, Int) -> String -> Int
-part1 slope = length . filter (=='#') . countTrees slope . parseGrid
+part1 slope = length . filter (=='#') . getObjectsForSlope slope . parseGrid
 
 part2 :: String -> Int
 part2 grid = product $ map (`part1` grid) [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+type Grid a = [[a]]
 
 parseGrid :: String -> [String]
 parseGrid = lines
@@ -15,14 +17,16 @@ getNextCoord (x, y) (slx, sly) width =
         nextX = (x + slx) `mod` width
     in (nextX, nextY)
 
-countTrees :: (Int, Int) -> [String] -> String
-countTrees = countTrees' (0, 0) 
+-- |Gets all objects on the grid in the path following a specified slope
+getObjectsForSlope :: (Int, Int) -> Grid a -> [a]
+getObjectsForSlope slope grid = getObjectsForSlope' (0, 0) slope (gridWidth grid) grid
     where
-        countTrees' :: (Int, Int) -> (Int, Int) -> [String] -> String
-        countTrees' c@(x, y) s grid 
+        getObjectsForSlope' :: (Int, Int) -> (Int, Int) -> Int -> Grid a -> [a]
+        getObjectsForSlope' c@(_, y) s w grid 
             | y >= length grid = []
-            | otherwise = 
-                let (nextX, nextY) = getNextCoord (x, y) s $ length (grid !! y)
-                in (grid !! y) !! x : countTrees' (nextX, nextY) s grid
-
+            | otherwise = grid `findElement` c : getObjectsForSlope' (getNextCoord c s w) s w grid
+        gridWidth :: Grid a -> Int
+        gridWidth g = length $ g !! 0
+        findElement :: Grid a -> (Int, Int) -> a
+        findElement grid (x, y) = (grid !! y) !! x
 
